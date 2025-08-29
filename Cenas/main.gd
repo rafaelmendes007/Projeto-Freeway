55ca6dbb94b3e3609e0ac3c564cbd0695f031421
Rafael Mendes <rafaeloms07@gmail.com>
2025-08-29 12:00:39 -0300

    Adicionei o placar para o player 2, e ajeitei a mensagem de vitória

--- Cenas/main.gd
+++ Cenas/main.gd
@@ -4,11 +4,13 @@ var cenas_carros = preload("res://Cenas/carros.tscn");
 var pistas_rapidas_y = [104, 272, 488]; # Possui uma distância de 56 pra cada
 var pistas_lentas_y = [160, 216, 324, 384, 438, 544, 600];
 var score = 0;
-
+var score2 = 0
+#.text = " ";
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	$HUD/Placar.text = str(score);
-	$HUD/Mensagem.text = " ";
+	$HUD/Placar2.text = str(score2);
+	$HUD/Mensagem.hide()
 	$HUD/Button.hide();
 	$AudioTema.play();
 	randomize();
@@ -30,20 +32,37 @@ func _on_timer_carros_lentos_timeout() -> void:
 	carro.set_linear_velocity(Vector2(randi_range(300, 320), 0));
 	carro.set_linear_damp(0.0);
 
-
+# CRIAR UMA MENSAGEM PRA CADA PLAYER
 func _on_player_pontua() -> void:
 	if score <= 10:
 		score += 1;
 		$HUD/Placar.text = str(score);
 		$AudioPonto.play();
-		$Player.position = $Player.posicao_inicial;
-	if score == 1:
+		$Player.position = $Player.posicao_inicial_p1;
+	if score == 2:
 		$HUD/Mensagem.show();
 		$HUD/Button.show();
 		$TimerCarrosRapidos.stop();
 		$TimerCarrosLentos.stop();
 		$AudioVitoria.play();
 		$Player.speed = 0;
+		$Player2.speed = 0;
+		
+func _on_player_2_pontua_2() -> void:
+	if score2 <= 10:
+		score2 += 1;
+		$HUD/Placar2.text = str(score2);
+		$AudioPonto.play();
+		$Player2.position = $Player2.posicao_inicial_p2;
+	if score2 == 2:
+		$HUD/Mensagem.show();
+		$HUD/Button.show();
+		$TimerCarrosRapidos.stop();
+		$TimerCarrosLentos.stop();
+		$AudioVitoria.play();
+		$Player2.speed = 0;
+		$Player.speed = 0;
+
 
 func _on_hud_reinicia() -> void:
-	get_tree().reload_current_scene()
+	get_tree().reload_current_scene();
--- Cenas/player.gd
+++ Cenas/player.gd
@@ -1,26 +1,45 @@
 extends Area2D;
 
 signal pontua;
+signal pontua2;
+
+# variável para identificar o jogador
+@export var player_id: int = 1;
 var speed: float = 300.0;
 var screen_size: Vector2;
-var posicao_inicial: Vector2 = Vector2(640, 690);
+var posicao_inicial_p1: Vector2 = Vector2(840, 685);
+var posicao_inicial_p2: Vector2 = Vector2(440, 685);
 
 # função para fazer o player retornar à posição inicial
 func _ready():
 	screen_size = get_viewport_rect().size;
-	position = posicao_inicial;
+	if player_id == 1:
+		position = posicao_inicial_p1;
+	if player_id == 2:
+		position = posicao_inicial_p2;
 
 func _process(delta):
 	var velocity = Vector2.ZERO;
 	
-	if Input.is_action_pressed("ui_up"):
-		velocity.y -= 1;
-	if Input.is_action_pressed("ui_down"):
-		velocity.y += 1;
-	if Input.is_action_pressed("ui_right"):
-		velocity.x += 1;
-	if Input.is_action_pressed("ui_left"):
-		velocity.x -= 1;
+	if player_id == 1:
+		if Input.is_action_pressed("ui_up"):
+			velocity.y -= 1;
+		if Input.is_action_pressed("ui_down"):
+			velocity.y += 1;
+		if Input.is_action_pressed("ui_right"):
+			velocity.x += 1;
+		if Input.is_action_pressed("ui_left"):
+			velocity.x -= 1;
+	
+	if player_id == 2:
+		if Input.is_action_pressed("p2_up"):
+			velocity.y -= 1;
+		if Input.is_action_pressed("p2_down"):
+			velocity.y += 1;
+		if Input.is_action_pressed("p2_right"):
+			velocity.x += 1;
+		if Input.is_action_pressed("p2_left"):
+			velocity.x -= 1;
 	
 	
 	if velocity !=Vector2.ZERO:
@@ -39,7 +58,13 @@ func _process(delta):
 
 func _on_body_entered(body: Node2D) -> void:
 	if body.name == "LinhaChegada":
-		emit_signal("pontua");
+		if player_id == 1:
+			emit_signal("pontua");
+		if player_id == 2:
+			emit_signal("pontua2");
 	else:
 		$Audio.play();
-		position = posicao_inicial;
+		if player_id == 1:
+			position = posicao_inicial_p1;
+		if player_id ==  2:
+			position = posicao_inicial_p2;
